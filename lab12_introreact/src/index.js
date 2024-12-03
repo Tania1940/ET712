ce85c0a7bf673a3d25fefb9536325976b76c355d
Tania1940 <158099343+Tania1940@users.noreply.github.com>
2024-12-03 15:56:26 +0000

    lab12 added

---
+++ lab12_introreact/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import landscape from './images/landscape.jpg'
+import './index.css'
+
+// content in the app
+const App = function(){
+  // creating a JSX variabel
+  const anymsg = "I am a text constant variable"
+  return(
+    <>
+    <h1 style={{color:'magenta', textAlign:'center'}}>Welcome to React JS</h1>
+    <h2 style={{fontFamily:'algerian'}} className='subtitle'>Tania Akthar</h2>
+    <figure style={{border:'inset olive 20px', width:'60%', margin:'auto'}}>
+      <img src={landscape} style={{width:'100%', display:'block'}}/>
+    </figure>
+    <h2 style={{padding:'2em'}}>{anymsg}</h2>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
