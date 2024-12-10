bbeadfe6991a648b56a134c1ba32c3f4649f8008
Tania1940 <158099343+Tania1940@users.noreply.github.com>
2024-12-10 19:50:53 +0000

    project 2

--- project_02/src/App.js
+++ project_02/src/App.js
@@ -1,24 +1,28 @@
-
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import routing files
-import Hermès from './components/Hermès'
-import LouisVuitton from './components/LouisVuitton'
-import Chanel from './components/Chanel'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Chanel from './pages/Chanel';
+import Hermes from './pages/Hermes';
+import LouisVuitton from './pages/LouisVuitton';
 
 function App() {
   return (
-    <>
-     <h1 className='title'>Luxury Bags</h1>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Hermès/>}/>
-      <Route path='/LouisVuitton' element={<LouisVuitton/>}/>
-      <Route path='/Chanel' element={<Chanel/>}/>
-    </Routes>
-  </BrowserRouter>
-  </>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/chanel">Chanel</Link>
+        <Link to="/hermes">Hermès</Link>
+        <Link to="/louis-vuitton">Louis Vuitton</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chanel" element={<Chanel />} />
+        <Route path="/hermes" element={<Hermes />} />
+        <Route path="/louis-vuitton" element={<LouisVuitton />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ project_02/src/pages/Hermes.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Card from '../components/Card';
+import ModalWindow from '../components/ModalWindow';
+
+const items = [
+  { title: 'Chanel Bag 1', image: '/images/chanel1.jpg', description: 'Classic quilted bag.' },
+  { title: 'Chanel Bag 2', image: '/images/chanel2.jpg', description: 'Timeless tote.' },
+  // Add 4 more items
+];
+
+function Chanel() {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  return (
+    <div>
+      <h1>Chanel Collection</h1>
+      <p>Discover iconic designs from Chanel.</p>
+      <div className="card-container">
+        {items.map((item, index) => (
+          <Card
+            key={index}
+            title={item.title}
+            image={item.image}
+            description={item.description}
+            onClick={() => setSelectedItem(item)}
+          />
+        ))}
+      </div>
+      {selectedItem && (
+        <ModalWindow
+          isOpen={!!selectedItem}
+          image={selectedItem.image}
+          title={selectedItem.title}
+          description={selectedItem.description}
+          onClose={() => setSelectedItem(null)}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Chanel;
